37e111f878f3de33f2bb7f664cc08868c940a70e
TalebMaham <sidnas9@gmail.com>
2023-10-30 20:19:46 +0100

    Un premier senario de teste

--- REPORT.md
+++ REPORT.md
@@ -1,39 +1,62 @@
-# Test Report - Express.js Application for Task (Todos) Management
+# Rapport de Test - API Todo
+
+Ce rapport documente les tests effectués sur l'API Todo pour garantir son bon fonctionnement.
 
 ## Introduction
-This test report examines the Express.js application designed to handle CRUD operations on tasks (todos). The goal is to identify potential issues and provide recommendations to enhance the application.
 
-## Areas for Improvement
+L'API Todo est une application de gestion de tâches qui expose des fonctionnalités telles que la création, la mise à jour, la suppression et la récupération de tâches.
 
-### 1. Input Data Validation
-- **Recommendation**: Add input data validation for the `POST` and `PUT` routes. Ensure that `title` and `completed` fields are present and in the correct format before proceeding with database operations. Utilize validation libraries such as `express-validator` if necessary.
+## Tests de Base de Données Vide
 
-### 2. Error Handling
-- **Recommendation**: Error handling appears to be in place with the `try/catch` clause. Ensure that the controller returns appropriate errors with relevant HTTP status codes when errors occur. For instance, on failed task creation, return a response with the HTTP code `400 Bad Request` and a meaningful error message.
+### Devrait récupérer zéro Todos (base de données vide)
 
-### 3. Authentication and Authorization
-- **Recommendation**: If the application requires authentication and authorization, ensure that these functionalities are properly implemented using libraries like Passport.js.
+**Description**: Ce test vérifie que l'API retourne un tableau vide lorsque la base de données est vide.
 
-### 4. Pagination and Handling Large Data
-- **Recommendation**: If the application is expected to handle large data sets, consider adding pagination to limit the number of results returned by the `GET` route.
+**Résultat**: Le test a réussi. L'API a retourné un tableau vide comme prévu.
 
-### 5. Unit and Integration Testing
-- **Recommendation**: Set up unit and integration tests for the application's routes and controller to ensure proper functionality. Utilize testing libraries such as Mocha, Chai, or Jest.
+## Tests de Création et Récupération
 
-### 6. Logging
-- **Recommendation**: Implement proper logging to facilitate error tracking and monitoring of operations performed by the application. Use logging libraries like Winston or Bunyan.
+### Devrait créer deux Todos
 
-### 7. Security
-- **Recommendation**: Follow security best practices, including data sanitization to prevent injection attacks. Configure security headers, including Content Security Policy (CSP) headers if needed.
+**Description**: Ce test vérifie que deux tâches peuvent être créées avec succès via l'API.
 
-### 8. CORS Handling
-- **Recommendation**: Configure CORS (Cross-Origin Resource Sharing) headers correctly to avoid security issues related to requests from different domains.
+**Résultat**: Le test a réussi. Deux tâches ont été créées et leurs IDs ont été enregistrés.
 
-### 9. Documentation
-- **Recommendation**: Add documentation to the API endpoints so that other developers know how to use them. Utilize tools like Swagger or JSDoc code comments.
+### Devrait récupérer deux Todos après création
 
-### 10. Optimization
-- **Recommendation**: If the application is intended for production use, consider optimizing the code for maximum performance. This may include using database indexes, caching frequently accessed data, and more.
+**Description**: Ce test vérifie que l'API retourne les deux tâches créées dans l'ordre par date.
+
+**Résultat**: Le test a réussi. L'API a retourné les deux tâches dans l'ordre attendu.
+
+## Tests de Mise à Jour
+
+### Devrait mettre à jour la première tâche
+
+**Description**: Ce test vérifie que la première tâche peut être mise à jour avec succès.
+
+**Résultat**: Le test a réussi. La première tâche a été mise à jour conformément aux attentes.
+
+### Devrait récupérer deux Todos après mise à jour
+
+**Description**: Ce test vérifie que l'API retourne les deux tâches après la mise à jour, avec la première tâche mise à jour en premier.
+
+**Résultat**: Le test a réussi. L'API a retourné les deux tâches dans l'ordre attendu après la mise à jour.
+
+## Tests de Suppression
+
+### Devrait supprimer la deuxième tâche
+
+**Description**: Ce test vérifie que la deuxième tâche peut être supprimée avec succès.
+
+**Résultat**: Le test a réussi. La deuxième tâche a été supprimée conformément aux attentes.
+
+### Devrait récupérer une tâche après suppression
+
+**Description**: Ce test vérifie que l'API retourne une seule tâche après la suppression de la deuxième tâche.
+
+**Résultat**: Le test a réussi. L'API a retourné une seule tâche après la suppression.
 
 ## Conclusion
-The Express.js application for task (todos) management shows promise, but there are important areas for improvement to ensure the application's reliability, security, and performance. Implementing the above recommendations will contribute to enhancing the overall quality of the application.
+
+Tous les tests ont réussi avec succès, démontrant ainsi le bon fonctionnement de l'API Todo pour la gestion des tâches.
+
--- cypress/e2e/Test.cy.ts
+++ cypress/e2e/Test.cy.ts
@@ -1,67 +1,102 @@
 /// <reference types="Cypress" />
-import TodoController from '../../dist/controllers/todos.controller';
 
 const apiRoute = 'http://localhost:3000/todos';
 
 describe('Tests Cypress pour l\'API Todo', () => {
-  const todoController = new TodoController();
+  let firstTodoId; // Variable globale pour stocker l'ID de la première tâche
+  let secondTodoId; // Variable globale pour stocker l'ID de la deuxième tâche
 
   beforeEach(() => {
+    // Aucune initialisation nécessaire pour les tests dans ce cas
   });
 
-  it('Devrait récupérer tous les Todos', () => {
-   
-      cy.request('GET', apiRoute)
-        .should((response) => {
-          expect(response.status).to.equal(200);
-          expect(response.body).to.be.an('array');
-          //expect(response.body).to.have.length(todos.length); 
-       
-        });
-  });
-
-  it('Devrait créer un nouveau Todo', () => {
-    const newTodo = {
-      title: 'Nouveau Todo',
-    };
-
-    cy.request('POST', apiRoute, newTodo)
+  it('Devrait récupérer zéro Todos (base de données vide)', () => {
+    cy.request('GET', apiRoute)
       .should((response) => {
-        expect(response.status).to.equal(201);
-        expect(response.body).to.include(newTodo);
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(0); // La base de données est vide
       });
   });
 
-  it('Devrait mettre à jour un Todo existant', () => {
-    const newTodo = {
-      title: 'Todo à mettre à jour',
-    };
+  it('Devrait créer deux Todos', () => {
+    // Crée la première tâche
+    cy.request('POST', apiRoute, { title: 'Première tâche' })
+      .should((response) => {
+        expect(response.status).to.equal(201);
+        firstTodoId = response.body._id; // Stocke l'ID de la première tâche
+      });
 
-    todoController.createTodo(newTodo).then((createdTodo) => {
-      const updatedTodo = {
-        title: 'Todo mis à jour',
-        completed: true,
-      };
-
-      cy.request('PUT', `${apiRoute}/${createdTodo._id}`, updatedTodo)
-        .should((response) => {
-          expect(response.status).to.equal(200);
-          expect(response.body).to.include(updatedTodo);
-        });
-    });
+    // Crée la deuxième tâche
+    cy.request('POST', apiRoute, { title: 'Deuxième tâche' })
+      .should((response) => {
+        expect(response.status).to.equal(201);
+        secondTodoId = response.body._id; // Stocke l'ID de la deuxième tâche
+      });
   });
 
-  it('Devrait supprimer un Todo existant', () => {
-    const newTodo = {
-      title: 'Todo à supprimer',
+  it('Devrait récupérer deux Todos après création', () => {
+    cy.request('GET', apiRoute)
+      .should((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(2); // Il devrait y avoir deux tâches après la création
+
+        // Vérifie l'ordre par date (du plus ancien au plus récent)
+        const sortedTodos = response.body.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+        expect(sortedTodos[0].title).to.equal('Première tâche');
+        expect(sortedTodos[1].title).to.equal('Deuxième tâche');
+        expect(sortedTodos[0].completed).to.be.false;
+        expect(sortedTodos[1].completed).to.be.false;
+      });
+  });
+
+  it('Devrait mettre à jour la première tâche', () => {
+    const updatedTodo = {
+      title: 'Tâche mise à jour',
+      completed: true,
     };
 
-    todoController.createTodo(newTodo).then((createdTodo) => {
-      cy.request('DELETE', `${apiRoute}/${createdTodo._id}`)
-        .should((response) => {
-          expect(response.status).to.equal(200);
-          expect(response.body).to.have.property('message', 'Todo supprimé avec succès');
-        });
-    });
+    cy.request('PUT', `${apiRoute}/${firstTodoId}`, updatedTodo)
+      .should((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.include(updatedTodo);
+      });
+  });
+
+  it('Devrait récupérer deux Todos après mise à jour', () => {
+    cy.request('GET', apiRoute)
+      .should((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(2); // Le nombre de tâches reste inchangé après la mise à jour
+
+        // Vérifie l'ordre par date (du plus ancien au plus récent)
+        const sortedTodos = response.body.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+        expect(sortedTodos[0].title).to.equal('Tâche mise à jour');
+        expect(sortedTodos[1].title).to.equal('Deuxième tâche');
+        expect(sortedTodos[0].completed).to.be.true;
+        expect(sortedTodos[1].completed).to.be.false;
+      });
+  });
+
+  it('Devrait supprimer la deuxième tâche', () => {
+    cy.request('DELETE', `${apiRoute}/${secondTodoId}`)
+      .should((response) => {
+        expect(response.status).to.equal(200);
+      });
+  });
+
+  it('Devrait récupérer une tâche après suppression', () => {
+    cy.request('GET', apiRoute)
+      .should((response) => {
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.length(1); // Il devrait rester une seule tâche après la suppression
+
+        // Vérifie que la tâche restante est celle qui n'a pas été supprimée
+        expect(response.body[0].title).to.equal('Tâche mise à jour');
+        expect(response.body[0].completed).to.be.true;
+      });
   });
 });
